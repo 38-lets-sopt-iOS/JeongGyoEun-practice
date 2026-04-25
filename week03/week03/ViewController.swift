6034b99ab5b2c4b64b8c9b0fdddf28f13b22e8b4
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2026-04-25 11:39:54 +0900

    chore :  초기 세팅 및 이미지 추가

---
+++ week03/week03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week03
+//
+//  Created by 정교은 on 4/25/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
